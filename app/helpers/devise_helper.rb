e261d6751bac0129f71a34d2e62919d2d6f44fcb
Anuradha <aravinarayanan@fws-che-lt-3176.freshworks-corp.com>
2020-01-07 15:56:03 +0530

    changed deprecated method

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,20 +1,14 @@
 module DeviseHelper
     def devise_error_messages!
-      return '' if resource.errors.empty?
-  
-      messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-      html = <<-HTML
-      <div class="alert alert-danger alert-dismissible fade show" role="alert">
-        <button type="button" class="close" data-dismiss="alert">
-          <span aria-hidden="true">&times;</span>
-        </button>
-        <strong>
-         #{pluralize(resource.errors.count, "error")} must be fixed
-        </strong>
-        #{messages}
-      </div>
-      HTML
-  
-      html.html_safe
-    end
+      ActiveSupport::Deprecation.warn <<-DEPRECATION.strip_heredoc
+      [Devise] `DeviseHelper.devise_error_messages!`
+      is deprecated and it will be removed in the next major version.
+      To customize the errors styles please run `rails g devise:views` and modify the
+      `devise/shared/error_messages` partial.
+    DEPRECATION
+
+    return "" if resource.errors.empty?
+
+    render "devise/shared/error_messages", resource: resource
+  end
   end
